36166a1875e6ca8ee2510da9932a4fc161e253a9
bklemfuss <bklemfuss@gmail.com>
2025-04-09 16:29:53 +0000

    Updated colors

--- lib/src/widgets/colors.dart
+++ lib/src/widgets/colors.dart
@@ -4,6 +4,6 @@ class AppColors {
   static const primary = Color.fromARGB(222, 245, 133, 81);
   static const secondary = Color.fromARGB(222, 245, 133, 81);
   static const background = Color.fromARGB(212, 29, 43, 59);
-  static const textPrimary = Color.fromARGB(255, 113, 113, 113);
-  static const textSecondary = Colors.black54;
+  static const textPrimary = Color.fromARGB(255, 250, 250, 250);
+  static const textSecondary = Color.fromARGB(255, 255, 255, 255);
 }
