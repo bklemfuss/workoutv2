05ac68ed281f6c303e2dc487e1e45cff119783f4
bklemfuss <bklemfuss@gmail.com>
2025-04-10 17:54:14 +0000

    Created theme folder and added platform themes and moved colors.dart to theme folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io' as io;
 import 'package:flutter/foundation.dart'; // For kIsWeb and defaultTargetPlatform
 import 'dart:io';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
